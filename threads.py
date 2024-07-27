6926515ba42b3f840955267a1a1cb197df4d8eae
Rudra Prakash <rudra.prakash1@delhivery.com>
2024-07-27 10:51:48 +0530

    PEP8 format

--- threads.py
+++ threads.py
@@ -1,17 +1,19 @@
 import threading
 import time
 
+
 def main():
     thread_fun()
     print("Execute after thread function")
 
+
 def thread_fun():
     threads = []
-    for i in range(0,10):
+    for i in range(0, 10):
         t = threading.Thread(target=each_thread, args=(i,))
         t.start()
         threads.append(t)
-    
+
     for t in threads:
         t.join()
 
@@ -19,6 +21,7 @@ def thread_fun():
 def each_thread(idx):
     time.sleep(5)
     print(f'Thread: {idx}')
+
 
 # Calling main function
 main()
